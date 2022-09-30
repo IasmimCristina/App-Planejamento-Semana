da4edb0e38b352b34360afdcdf18c995ee0a2db0
IasmimCristina <iaasmimcristinaa@gmail.com>
2022-09-30 10:42:48 -0300

    Estilização da sessão de inputs e dropdowns - Scroll inicial.

--- assets/JS/showDateTime.js
+++ assets/JS/showDateTime.js
@@ -11,6 +11,8 @@ const tempo = setInterval(function time() {
 
   //Adição do 0 à esquerda.
   if (horas < 10) horas = '0' + horas;
+  if (minutos< 10) minutos = '0' + minutos;
+
   let tempo = horas + ":" + minutos;
   currentTime.textContent = tempo;
 })
