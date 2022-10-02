f6ad4f759638ed13f1452ee3590f98f10a823c0b
IasmimCristina <iaasmimcristinaa@gmail.com>
2022-10-02 12:48:20 -0300

    Criação de todas as abas dos dias de semana. Início da seleção das tabs com JS.

---
+++ assets/JS/takingTask.js
@@ -0,0 +1,22 @@
+// day-of-the-week  atividade  task-time  || btn-salvar-task btn-excluir-task
+
+
+
+
+
+
+// const btnAdd = document.querySelector(".btn-salvar-task");
+
+// btnAdd.addEventListener("click", function(e)  {
+//   e.preventDefault();
+    
+//     const atividade = document.querySelector("#atividade");
+
+//     const valueAtividade = atividade.value;
+
+//     console.log(valueAtividade);
+// });
+
+// var taskTime = document.querySelector(".task-input-time");
+// var taskTimeText = taskTime.value;
+// console.log(taskTimeText);
